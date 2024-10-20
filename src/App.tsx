794dab889a86f26b5e907e1daea3a01b1a6e9a9a
Golam Muntakim <seams123456789@gmail.com>
2024-10-20 11:21:58 -0700

    routing set up done

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
-
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Header from './Components/Header'
+import {lazy, Suspense} from 'react';
+import Loader from './Components/Loader';
+const Home = lazy(()=>import("./Components/Home"))
+const Learning = lazy(()=>import("./Components/Learning"))
+const Quize = lazy(()=>import("./Components/Quize"))
+const Result = lazy(()=>import("./Components/Result"))
+const Login = lazy(()=>import("./Components/Login"))
 
 function App() {
-
-
   return (
-    <>
-      
-    </>
+  <Router>
+    <Header></Header>
+   <Suspense fallback={<Loader></Loader>}>
+   <Routes>
+      <Route path='/' element={<Home></Home>}/>
+      <Route path='/learn' element={<Learning/>}/>
+      <Route path='/quize' element={<Quize/>}/>
+      <Route path='/result' element={<Result/>}/>
+      <Route path='/login' element={<Login/>}/>
+    </Routes>
+   </Suspense>
+  </Router>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-
+import {ThemeProvider} from '@emotion/react'
+import { createTheme, CssBaseline } from '@mui/material'
+import { Palette } from '@mui/icons-material'
+const theme = createTheme({
+  palette:{
+    primary:{
+      main:"rgb(255,0,0)",
+    },
+  },
+})
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+   <ThemeProvider theme={theme}>
+    <CssBaseline/>
+   <App />
+   </ThemeProvider>
   </StrictMode>,
 )
